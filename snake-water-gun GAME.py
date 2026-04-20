a12b7993197147b0bea3d060049e635a42d9b8a1
Arquam Ali Shakeeb <arquamalishakeeb@gmail.com>
2026-04-20 07:50:03 +0500

    Update snake-water-gun GAME.py

--- snake-water-gun GAME.py
+++ snake-water-gun GAME.py
@@ -5,14 +5,14 @@ comp = random.randint(0,2)
  
 user = int(input("Enter 0 for Snake , 1 for Water and 2 for Gun: "))
 
-def check (comp,user):
+def check (comp, user):
     if comp == 0 and user == 1:
         print(f"computer choses : {comp}")
         print(f"You choses : {user}")
         print("Snake beats Water" "\n" "you lose")
     elif comp == 1 and user == 2:
         print(f"computer choses : {comp}")
-        print(f"You choses : {user}")
+        print(f"You choses : {user}") 
         print("Water beats Gun" "\n" "you lose")
     elif comp == 2 and user == 0:
         print(f"computer choses : {comp}")
